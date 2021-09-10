e2820a8b5066962d6f05d420e10db1ecf6cb88f2
c.henrique.eugenio@accenture.com <caue_eugenio@hotmail.com>
2021-09-10 18:27:22 -0300

    10-09-21: criacao componente photo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
-  image = 'https://magazinefeminina.com.br/wp-content/uploads/2020/09/wp4355353.jpg';
-  description = "sabre de luz azul";
+
 }

74cf7e76659ce57292eb8838785f63b01a3369ed
c.henrique.eugenio@accenture.com <caue_eugenio@hotmail.com>
2021-09-11 00:04:00 -0300

    11-09-21: organizacao dos modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+
+  photos = [
+    {
+      "image": 'https://magazinefeminina.com.br/wp-content/uploads/2020/09/wp4355353.jpg',
+      "description": 'sabre de luz azul'
+    },
+    {
+      "image": 'https://wallpaperaccess.com/full/150242.jpg',
+      "description": 'sabre de luz vermelho'
+    },
+    {
+      "image": 'https://i.pinimg.com/564x/05/7d/44/057d44fa0389f7f96d62421356a81ebc.jpg',
+      "description": 'sabre de luz amarelo'
+    },
+    {
+      "image": 'https://i.pinimg.com/originals/6f/1b/69/6f1b6988545105b9a9bcb8e3c651250e.png',
+      "description": 'sabre de luz verde'
+    }
+
+
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PhotoComponent } from './components/photo/photo.component';
+import { PhotosModule } from './components/photos/photos.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PhotoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
